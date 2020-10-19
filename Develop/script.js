6e0860a97ae4d6b5db17b9d4930093569a40da8f
Green S <kaylasierrabartley@gmail.com>
2020-10-18 23:13:24 -0500

    added generatePassword function and added character type arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment code here
-
+//function to generate and return password
+function generatePassword(){
+  //arrays of character types
+  var a = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+  var b = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  var d = ["+", "-", "%", "_", "!", "(", ")", "{", "}", "[", "]", "^","~", "*", "?", ":", "@", "/","\\", "\'", "#", "$", ",", "." ];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
